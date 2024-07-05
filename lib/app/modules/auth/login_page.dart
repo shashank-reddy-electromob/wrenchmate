209208d572f89fbff5305cf2b4384185a0a9bb00
Aryan Srivastava <srivastavaaryan97497@gmail.com>
2024-07-05 21:17:11 +0530

    login screen UI

--- lib/app/modules/auth/login_page.dart
+++ lib/app/modules/auth/login_page.dart
@@ -9,6 +9,7 @@ class LoginPage extends StatefulWidget {
 
 class _LoginPageState extends State<LoginPage> {
   final _phonenumbercontroller = TextEditingController();
+  String _selectedCountryCode = '+91';
 
   void _login() {
     final AuthController controller = Get.find();
@@ -24,28 +25,185 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Login')),
+      backgroundColor: Colors.white,
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            TextField(
-              controller: _phonenumbercontroller,
-              decoration: InputDecoration(
-                labelText: 'Username',
+        child: SingleChildScrollView(scrollDirection: Axis.vertical,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(height: 128,),
+              //logo
+              Image.asset(
+                'assets/images/wrench_mate_logo.png',
+                fit: BoxFit.cover,
               ),
-            ),
-            SizedBox(height: 32.0),
-            ElevatedButton(
-              onPressed: _login,
-              child: Text('Login'),
-            ),
-            ElevatedButton(
-              onPressed: _googlelogin,
-              child: Text('google'),
-            ),
-          ],
+              SizedBox(height: 32.0),
+              //text
+              Text("Log In",style: TextStyle(fontSize: 22,fontWeight: FontWeight.bold,color: Color(0xff120D26)),),
+              SizedBox(height: 12.0),
+              Text("Hello, Welcome back to your account.",style: TextStyle(fontSize: 16,color: Color(0xff969696)),),
+              //textfield
+              SizedBox(height: 24.0),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 12),
+                child: Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(8.0),
+                    color: Colors.transparent,
+                  ),
+                  child: Row(
+                    children: [
+                      // DropdownButtonHideUnderline(
+                      //   child: DropdownButton<String>(
+                      //     value: _selectedCountryCode,
+                      //     items: [
+                      //       DropdownMenuItem<String>(
+                      //         value: '+91',
+                      //         child: Row(
+                      //           children: [
+                      //             ClipOval(
+                      //               child: Image.asset(
+                      //                 'assets/images/indian_flag.png',
+                      //                 width: 24,
+                      //                 height: 24,
+                      //                 fit: BoxFit.cover,
+                      //               ),
+                      //             ),
+                      //             SizedBox(width: 8),
+                      //             Text('+91',style: TextStyle(color: Colors.grey, fontSize: 20.0),),
+                      //           ],
+                      //         ),
+                      //       ),
+                      //       DropdownMenuItem<String>(
+                      //         value: '+1',
+                      //         child: Row(
+                      //           children: [
+                      //             ClipOval(
+                      //               child: Image.asset(
+                      //                 'assets/images/indian_flag.png',
+                      //                 width: 24,
+                      //                 height: 24,
+                      //                 fit: BoxFit.cover,
+                      //               ),
+                      //             ),
+                      //             SizedBox(width: 8),
+                      //             Text('+1',style: TextStyle(color: Colors.grey, fontSize: 20.0),),
+                      //           ],
+                      //         ),
+                      //       ),
+                      //     ],
+                      //     onChanged: (String? newValue) {
+                      //       setState(() {
+                      //         _selectedCountryCode = newValue!;
+                      //       });
+                      //     },
+                      //   ),
+                      // ),
+                      Expanded(
+                        child: TextField(
+                          controller: _phonenumbercontroller,
+                          keyboardType: TextInputType.number,
+                          decoration: InputDecoration(
+                            hintText: 'Enter number with +91',
+                            hintStyle: TextStyle(color: Colors.grey, fontSize: 22.0), // Increased hint text size
+                            border: OutlineInputBorder(
+                              borderSide: BorderSide.none,
+                            ),
+                            filled: true,
+                            fillColor: Colors.transparent,
+                          ),
+                          style: TextStyle(fontSize: 22.0), // Increased entered text size
+                        )
+
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              SizedBox(height: 32.0),
+              //submit
+              Container(width: MediaQuery.of(context).size.width*0.8,height: 60,
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    foregroundColor: Colors.blue, backgroundColor: Color(0xff1671D8), // Text color
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(16.0),
+                    ),
+                  ),
+                  onPressed:
+                    _login,
+                  child: Text(
+                    'REQUEST OTP',
+                    style: TextStyle(
+                      color: Colors.white,fontSize: 20
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: 32.0),
+              Text("OR",style: TextStyle(fontSize: 22,fontWeight: FontWeight.bold,color: Color(0xff969696)),),
+              SizedBox(height: 32.0),
+              //google
+              Container(width: MediaQuery.of(context).size.width*0.8,height: 60,
+                child: ElevatedButton(
+
+                  style: ElevatedButton.styleFrom(elevation: 2,
+                    foregroundColor: Colors.white, backgroundColor: Colors.white, // Text color
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(16.0),
+                    ),
+                  ),
+                  onPressed:
+                    _googlelogin,
+                  child: Row(mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Image.asset(
+                        'assets/images/google logo.png',
+                        fit: BoxFit.cover,
+                      ),
+                      SizedBox(width: 8,),
+                      Text(
+                        'Login With Google',
+                        style: TextStyle(
+                          color: Color(0xff120D26),fontSize: 20
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              SizedBox(height: 16.0),
+              //facebook
+              Container(width: MediaQuery.of(context).size.width*0.8,height: 60,
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    elevation: 2,
+                    foregroundColor: Colors.white, backgroundColor: Colors.white, // Text color
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(16.0),
+                    ),
+                  ),
+                  onPressed:(){},
+                  child: Row(mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Image.asset(
+                        'assets/images/facebok_logo.png',
+                        fit: BoxFit.cover,
+                      ),
+                      SizedBox(width: 8,),
+                      Text(
+                        'Login With Facebook',
+                        style: TextStyle(
+                            color: Color(0xff120D26),fontSize: 20
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
