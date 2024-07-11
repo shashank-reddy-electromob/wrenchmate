e27160e6f1fef5013fa2a0f9a5401a7d88aa67e2
Aryan Srivastava <srivastavaaryan97497@gmail.com>
2024-07-11 19:31:02 +0530

    drawer logic implemented.

---
+++ lib/app/modules/home/drawer.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class drawerPage extends StatefulWidget {
+  const drawerPage({super.key});
+
+  @override
+  State<drawerPage> createState() => _drawerPageState();
+}
+
+class _drawerPageState extends State<drawerPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(color: Colors.white60,);
+  }
+}
--- lib/app/modules/home/home_page.dart
+++ lib/app/modules/home/home_page.dart
@@ -7,6 +7,7 @@ import 'package:wrenchmate_user_app/app/modules/home/widgits/services.dart';
 import 'package:wrenchmate_user_app/app/modules/home/widgits/toprecommendedservices.dart';
 import 'package:wrenchmate_user_app/app/routes/app_routes.dart';
 import '../../controllers/home_controller.dart';
+import 'drawer.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -17,79 +18,166 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   //final user = FirebaseAuth.instance.currentUser!;
+  double xOffSet = 0;
+  double yOffSet = 0;
+  double scaleFactor = 1;
+  bool isDrawerOpen = false;
+
+  void _onHorizontalDragUpdate(DragUpdateDetails details) {
+    if (details.primaryDelta! > 0) {
+      // Swiping in right direction
+      setState(() {
+        xOffSet = 230;
+        yOffSet = MediaQuery.of(context).size.height * 0.2;
+        scaleFactor = 0.6;
+        isDrawerOpen = true;
+      });
+    } else if (details.primaryDelta! < 0) {
+      // Swiping in left direction
+      setState(() {
+        xOffSet = 0;
+        yOffSet = 0;
+        scaleFactor = 1;
+        isDrawerOpen = false;
+      });
+    }
+  }
+
+  void _onTap() {
+    setState(() {
+      xOffSet = 0;
+      yOffSet = 0;
+      scaleFactor = 1;
+      isDrawerOpen = false;
+    });
+  }
   @override
   Widget build(BuildContext context) {
     final HomeController controller = Get.find();
     return Scaffold(
-      appBar: AppBar(
-        title: Row(
+      body: Stack(
         children: [
-          ClipOval(
-            child: Image.asset(
-              'assets/images/weekend.png',
-              fit: BoxFit.cover,
-              height: 45.0,
-              width: 45.0,
-            ),
-          ),
-          SizedBox(width: 10),
-          Container(
-            height: 45,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  //"Hi ${user.phoneNumber}",
-                  "Hi babe",
-                  style:
-                  TextStyle(fontSize: 22, color: Colors.black),
-                ),
-                Row(
+          drawerPage(),
+          GestureDetector(
+            onHorizontalDragUpdate: _onHorizontalDragUpdate,
+            onTap: isDrawerOpen ? _onTap : null,
+            child: AnimatedContainer(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                color: Colors.white,
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black.withOpacity(0.1),
+                    spreadRadius: 10,
+                    blurRadius: 25,
+                  ),
+                ],
+              ),
+              duration: Duration(microseconds: 100),
+              transform: Matrix4.translationValues(xOffSet, yOffSet, 0)
+                ..scale(scaleFactor),
+              child: SingleChildScrollView(
+                scrollDirection: Axis.vertical,
+                child: Column(
                   children: [
-                    Icon(Icons.location_on_outlined,size: 16,color: Color(0xffFF5402),),
-                    Text("234, FTS Colony, HYD",
-                        style: TextStyle(
-                            fontSize: 16, color: Colors.black)),
+                    Container(
+                      height: 50,
+                    ),
+                    Container(
+                      margin: EdgeInsets.symmetric(horizontal: 16),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Row(
+                            children: [
+                              GestureDetector(
+                                onTap: () {
+                                  setState(() {
+                                    xOffSet = 230;
+                                    yOffSet =
+                                        MediaQuery.of(context).size.height *
+                                            0.2;
+                                    scaleFactor = 0.6;
+                                  });
+                                },
+                                child: ClipOval(
+                                  child: Image.asset(
+                                    'assets/images/weekend.png',
+                                    fit: BoxFit.cover,
+                                    height: 45.0,
+                                    width: 45.0,
+                                  ),
+                                ),
+                              ),
+                              SizedBox(width: 10),
+                              Container(
+                                height: 45,
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      //"Hi ${user.phoneNumber}",
+                                      "Hi babe",
+                                      style: TextStyle(
+                                          fontSize: 22, color: Colors.black),
+                                    ),
+                                    Row(
+                                      children: [
+                                        Icon(
+                                          Icons.location_on_outlined,
+                                          size: 16,
+                                          color: Color(0xffFF5402),
+                                        ),
+                                        Text("234, FTS Colony, HYD",
+                                            style: TextStyle(
+                                                fontSize: 16,
+                                                color: Colors.black)),
+                                      ],
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                          Container(
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(12),
+                              border: Border.all(
+                                color: Color(
+                                    0xffE7E7E7), // Set the border color to grey
+                                width: 1.0, // Set the border width
+                              ),
+                            ),
+                            child: IconButton(
+                              icon: Icon(
+                                Icons.notifications_none_outlined,
+                                color: Color(0xff515151),
+                              ),
+                              onPressed: () {
+                                Get.toNamed(AppRoutes.NOTIFICATIONS);
+                              },
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    SizedBox(
+                      height: 12,
+                    ),
+                    searchbar(),
+                    SizedBox(
+                      height: 12,
+                    ),
+                    offersSliders(),
+                    serviceswidgit(),
+                    toprecommendedservices(),
                   ],
                 ),
-              ],
-            ),
-          )
-        ],
-      ),
-        actions: [
-          //notification button
-          Container(
-          decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.circular(12),
-            border: Border.all(
-              color: Color(0xffE7E7E7), // Set the border color to grey
-              width: 1.0, // Set the border width
+              ),
             ),
           ),
-          child: IconButton(
-            icon: Icon(Icons.notifications_none_outlined,color: Color(0xff515151),),
-            onPressed:(){
-              Get.toNamed(AppRoutes.NOTIFICATIONS);
-            },
-          ),
-        ),
-          Container(width: 20,)
         ],
-      ),
-      body: SingleChildScrollView(
-        scrollDirection: Axis.vertical,
-        child: Column(
-          children: [
-            SizedBox(height: 12,),
-            searchbar(),
-            SizedBox(height: 12,),
-            offersSliders(),
-            serviceswidgit(),
-            toprecommendedservices(),
-          ],
-        ),
       ),
     );
   }
