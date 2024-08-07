90b530ab07a24b5dd93831f48a2eb225dd0baf21
Shubhangi Mishra <sonalestilo@gmail.com>
2024-08-07 19:16:38 +0530

    Map screen added

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:wrenchmate_user_app/app/Map%20screens/MapScreen.dart';
 import 'app/bindings/initial_binding.dart';
 import 'app/routes/app_pages.dart';
 import 'firebase_options.dart';
@@ -20,6 +21,7 @@ class MyApp extends StatelessWidget {
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
       initialBinding: InitialBinding(),
+      // home: MapScreen(),
       initialRoute: FirebaseAuth.instance.currentUser == null ? AppPages.INITIAL : AppPages.CAR_REGISTER,
       getPages: AppPages.routes,
     );
