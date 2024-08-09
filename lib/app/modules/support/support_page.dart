835fb84ab4ce3ca0b9e329f3dd34e0def5a0701a
Aryan Srivastava <srivastavaaryan97497@gmail.com>
2024-08-09 23:10:09 +0530

    socials page done, faq copy paste remaining, and chatbox....fml

--- lib/app/modules/support/support_page.dart
+++ lib/app/modules/support/support_page.dart
@@ -1,14 +1,67 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:wrenchmate_user_app/app/modules/support/widgets/socialmediatabs.dart';
 import '../../controllers/support_controller.dart';
+import '../booking/widgets/tabButton.dart';
 
-class SupportPage extends StatelessWidget {
+class SupportPage extends StatefulWidget {
+  @override
+  State<SupportPage> createState() => _SupportPageState();
+}
+
+class _SupportPageState extends State<SupportPage> {
+  String selectedTab='FAQ';
   @override
   Widget build(BuildContext context) {
     final SupportController controller = Get.find();
-    return Scaffold(
-      appBar: AppBar(title: Text('Support')),
-      body: Center(child: Text('Support Page')),
+    return Scaffold(backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        title: Text(
+          'Help and Support',
+          style: TextStyle(fontWeight: FontWeight.w500, fontSize: 24),
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal:  18.0),
+        child: Column(
+          children: [
+            //tabs
+            Row(
+              children: [
+                // Current booking
+                TabButton(
+                  text: 'FAQ',
+                  isSelected: selectedTab == 'FAQ',
+                  onTap: () {
+                    setState(() {
+                      selectedTab = 'FAQ';
+                    });
+                  },
+                ),
+                SizedBox(width: 8),
+                // History
+                TabButton(
+                  text: 'Contact Us',
+                  isSelected: selectedTab == 'ContactUs',
+                  onTap: () {
+                    setState(() {
+                      selectedTab = 'ContactUs';
+                    });
+                  },
+                ),
+              ],
+            ),
+            SizedBox(height: 8,),
+            //ciontact us
+            CustomContainer(imagePath: 'assets/socials/chatus.png', text: "Chat with Us", onTap: (){}),
+            CustomContainer(imagePath: 'assets/socials/facebook.png', text: "Facebook", onTap: (){}),
+            CustomContainer(imagePath: 'assets/socials/instagram.png', text: "Instagram", onTap: (){}),
+            CustomContainer(imagePath: 'assets/socials/twt.png', text: "Twitter", onTap: (){}),
+
+          ],
+        ),
+      )
     );
   }
 }
---
+++ lib/app/modules/support/widgets/socialmediatabs.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+
+class CustomContainer extends StatelessWidget {
+  final String imagePath;
+  final String text;
+  final VoidCallback onTap;
+
+  const CustomContainer({
+    Key? key,
+    required this.imagePath,
+    required this.text,
+    required this.onTap,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric( vertical:  12.0),
+      child: GestureDetector(
+        onTap: onTap,
+        child: Container(
+          height: 72,
+          width: double.infinity,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(12),
+            color: Colors.white,
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black.withOpacity(0.2),
+                spreadRadius: 0,
+                blurRadius: 2,
+                offset: Offset(0, 1), // changes position of shadow
+              ),
+            ],
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Row(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(12.0),
+                    child: ClipOval(
+                      child: Image.asset(
+                        imagePath,
+                        width: 50,
+                        height: 50,
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+                  Text(
+                    text,
+                    style: const TextStyle(
+                      fontSize: 18,
+                      color: Color(0xff1F1F1F),
+                      fontWeight: FontWeight.w500
+                    ),
+                  ),
+                ],
+              ),
+              const Padding(
+                padding: EdgeInsets.only(right: 16.0),
+                child: Icon(
+                  Icons.chevron_right,
+                  color: Colors.black,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
